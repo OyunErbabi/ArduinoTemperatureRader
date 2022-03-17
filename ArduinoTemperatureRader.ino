ca819e627eaa7ac87f62b81bf23004f2c70f29f5
OyunErbabi <mehmet1992@hotmail.com>
2022-03-17 03:20:13 +0300

    SensorReadDone

--- ArduinoTemperatureRader.ino
+++ ArduinoTemperatureRader.ino
@@ -1,9 +1,42 @@
-void setup() {
-  // put your setup code here, to run once:
+  int pin_lm35 = A0;
+  int value_lm35 = 0;
+  float temperature = 0;
+  float temperature_voltage = 0;
 
-}
+  float final_temperature = 0;
 
-void loop() {
-  // put your main code here, to run repeatedly:
+  void setup() 
+  {
+    Serial.begin(9600);
+  }
 
-}
+  void loop() 
+  {
+    calculateTemperature();
+    printValues();       
+    delay(1000);
+  }
+
+  void calculateTemperature()
+  {
+    final_temperature = 0;  
+    for(int i = 0; i < 10; i++) 
+    {
+       value_lm35 = analogRead(pin_lm35);
+       temperature_voltage = (value_lm35/1023.0)*5000;
+       temperature = temperature_voltage/10.0;
+       final_temperature += temperature;
+       delay(10);
+    }     
+  }
+
+  void printValues()
+  {
+    Serial.print("Lm35 Value:  ");
+    Serial.println(value_lm35);
+    Serial.print("Voltage:  ");
+    Serial.println(temperature_voltage);
+    Serial.print("Temperature:  ");
+    Serial.println(final_temperature/10,1);
+    Serial.println("**********************************");
+  }
